19779d0f63d755947584318ba63cf37a02b0d1cc
Scott Maxwell <scott@scotteh.me>
2016-02-21 12:08:45 +0000

    fixed the shit

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Jordan smells kind of really good. Sometimes. Okay. ayy lmaoo good'
-      + 'haAUHWIJOIJWDIUHASDHIUha fun FUN FUN FUN timeiruehieuhs');
+  res.send('Jordan smells.');
 });
 
 app.listen(3000, function () {
