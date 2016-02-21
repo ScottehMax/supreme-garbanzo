9d644e3afc44a4a3e8ad65aa1da5a2c05aac9901
Scott Maxwell <scott@scotteh.me>
2016-02-21 08:03:32 +0000

    hi

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Jordan smells kind of really good. Sometimes. Okay. ayy lmao');
+  res.send('Jordan smells kind of really good. Sometimes. Okay. ayy lmaoooooo');
 });
 
 app.listen(3000, function () {
